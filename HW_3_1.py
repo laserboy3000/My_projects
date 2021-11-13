77335d91188fa26f6c35fb9673f6b78870fccca4
laserboy3000 <93968311+laserboy3000@users.noreply.github.com>
2021-11-14 01:21:51 +0300

    Update HW_3_1.py

--- HW_3_1.py
+++ HW_3_1.py
@@ -8,4 +8,3 @@ elif c>a and c>b and c>d:
     print("Largest of four numbers from", a, b, c, d, "is", c, type(c))
 else:
     print("Largest of four numbers from", a, b, c, d, "is", d, type(d))
-    print("Largest of four numbers from", a, b, c, d, "is", d, type(d))
